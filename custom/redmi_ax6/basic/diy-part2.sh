8b2a69de0f909e71eb917305630946cbdbf2ddac
sdf8057 <54014465+sdf8057@users.noreply.github.com>
2022-11-19 17:33:47 +0800

    modify ax6_basic

--- custom/redmi_ax6/basic/diy-part2.sh
+++ custom/redmi_ax6/basic/diy-part2.sh
@@ -12,3 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify some apps
+rm -rf feeds/luci/applications/luci-app-cpufreq
+rm -rf feeds/luci/applications/luci-app-ddns
+rm -rf feeds/luci/applications/luci-app-openvpn-server
+
+git clone https://github.com/sdf8057/modify_apps.git package/modify_apps
+
+mv package/modify_apps/luci-app-cpufreq feeds/luci/applications/luci-app-cpufreq
+mv package/modify_apps/luci-app-ddns feeds/luci/applications/luci-app-ddns
+mv package/modify_apps/luci-app-openvpn-server feeds/luci/applications/luci-app-openvpn-server
+
+echo 'refresh feeds'
+./scripts/feeds update -a
+./scripts/feeds install -a
