6ace19f5907943d41544c3105c1b3eaea854ec79
Zyilin98 <Zyilin98@zyilin98.cc>
2023-05-31 23:29:38 +0800

    规范格式

---
+++ custom/cmiot_ax18/lean/diy-part2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify version
+#sed -i 's/R22.11.11/V22.11.11/g' package/addition/default-settings/files/99-default-settings
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+git clone https://github.com/flytosky-f/luci-app-vlmcsd.git package/luci-app-vlmcsd
